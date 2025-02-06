36ef0f3817c0b7e4198c68ace18b99801352d33b
Mohammad F Rabby <rabbyxq@gmail.com>
2025-02-07 00:57:33 +0600

    Update install-dep.sh

--- install-dep.sh
+++ install-dep.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-# install python
-sudo apt-get update
-sudo apt-get install python2.7 python-pip python-dev
+
 
 # install numpy
-sudo apt-get install python-numpy
+brew install python-numpy
 
 # install apktool
 sudo cp -n ./bin/apktool* /usr/local/bin/
